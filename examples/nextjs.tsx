e88b066bea90bd3eb7272c91ac2542e101e83023
Charis Zhang <charisjzhang@berkeley.edu>
2024-07-15 17:30:36 -0700

    meh

--- examples/nextjs.tsx
+++ examples/nextjs.tsx
@@ -1,24 +1,22 @@
-"use client"
+"use client";
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { getFingerprint } from "@thumbmarkjs/thumbmarkjs";
 
 function Fingerprint() {
-    const [fingerprint, setFingerprint] = useState('');
-  
-    useEffect(() => {
-      getFingerprint()
-        .then((result) => {
-          setFingerprint(result);
-        })
-        .catch((error) => {
-          console.error('Error getting fingerprint:', error);
-        });
-    }, []);
-    
-    return (
-      <>{fingerprint}</>
-    );
-  }
+  const [fingerprint, setFingerprint] = useState("");
 
-export default Fingerprint
+  useEffect(() => {
+    getFingerprint()
+      .then((result) => {
+        setFingerprint(result);
+      })
+      .catch((error) => {
+        console.error("Error getting fingerprint:", error);
+      });
+  }, []);
+
+  return <>{fingerprint}</>;
+}
+
+export default Fingerprint``;
